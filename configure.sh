6ebb82946c1c7af4590488a0ebad770af741910d
SHTrassEr <shtrasser@gmail.com>
2014-01-12 13:27:51 +0900

    fix make error

--- configure.sh
+++ configure.sh
@@ -31,7 +31,8 @@ searchpaths()
 
   if [ ! -f "$DIR"/$FILE ]
   then 
-    echo "Warning: File '$FILE' was not found in any of ${DIRS[@]}. Please enter path to monodevelop."
+    echo "Warning: File '$FILE' was not found in any of ${DIRS[@]}."
+    echo "Please enter path to '$FILE':"
     read DIR
   fi
   RESULT=$DIR
@@ -41,28 +42,75 @@ searchpaths()
 # Find all paths that we need in order to generate the make file. Paths
 # later in the list are preferred.
 
-PATHS=( /opt/mono/lib/monodevelop /usr/lib/monodevelop /usr/local/monodevelop/lib/monodevelop /usr/local/lib/monodevelop /Applications/MonoDevelop.app/Contents/MacOS/lib/monodevelop '/Applications/Xamarin Studio.app/Contents/MacOS/lib/monodevelop')
-searchpaths "MonoDevelop" bin/MonoDevelop.Core.dll PATHS[@]
-MDDIR=$RESULT
-echo "Successfully found MonoDevelop root directory." $MDDIR
+MONO=mono
 
-echo "Running $MDDIR/monodevelop to determine MonoDevelop version"
-
-if [  -f "$MDDIR"/monodevelop ] 
-then 
-  # e.g. 3.0.4.7
-  MDVERSION4=`$MDDIR/bin/monodevelop /? | head -n 1 | grep -o "[0-9]\+.[0-9]\+.[0-9]\+\(.[0-9]\+\)\?"`
-  # e.g. 3.0.4
-  MDVERSION3=`$MDDIR/bin/monodevelop /? | head -n 1 | grep -o "[0-9]\+.[0-9]\+.[0-9]\+"`
-  echo "Detected MonoDevelop version " $MDVERSION4
-else
-  MDVERSION4=4.0.0.0
-  MDVERSION3=4.0
-  echo "Assumed MonoDevelop version " $MDVERSION4
+if ! hash $MONO 2>/dev/null;  then
+  echo "Warning: Mono was not found. Please enter path to mono."
+  read MONO
 fi
 
+if hash $MONO 2>/dev/null;  then
+  echo "Successfully found $MONO"
+else 
+  echo "Warning: Mono was not found. Please enter path to mono."
+  read MONO
+
+  if [ -f $MONO ]
+  then
+    echo "Successfully found $MONO"
+  else
+    echo "Configuration error. Mono was not found."
+    exit
+  fi
+fi
+
+PATHS=( 
+  /opt/mono/lib/monodevelop/bin 
+  /usr/lib/monodevelop/bin 
+  /usr/local/monodevelop/lib/monodevelop/bin 
+  /usr/local/lib/monodevelop/bin 
+  /Applications/MonoDevelop.app/Contents/MacOS/lib/monodevelop/bin 
+  /Applications/Xamarin Studio.app/Contents/MacOS/lib/monodevelop/bin
+  )
+searchpaths "MonoDevelop" MonoDevelop.Core.dll PATHS[@]
+MDDIR=$RESULT
+
+if [ -f $MDDIR/MonoDevelop.Core.dll ]
+then
+  echo "Successfully found MonoDevelop bin directory: $MDDIR"
+else
+  echo "Configuration error. File $MDDIR/MonoDevelop.Core.dll was not found."
+  exit
+fi
+
+MDRUNFILE="$MDDIR/MonoDevelop.exe"
+
+if [ -f "$MDRUNFILE" ] 
+then 
+  echo "Running $MONO $MDRUNFILE to determine MonoDevelop version"
+  # e.g. 3.0.4.7
+  MDVERSION=`$MONO $MDRUNFILE /? | head -n 1 | grep -o "[0-9]\+.[0-9]\+\(.[0-9]\+\)\?"`
+  echo "Detected MonoDevelop version " $MDVERSION
+else
+  MDVERSION=4.0
+  echo "Assumed MonoDevelop version " $MDVERSION
+fi
+
+PATHS=( $MDDIR )
+
+searchpaths "mdtool" mdtool.exe PATHS[@]
+MDTOOL=$RESULT/mdtool.exe
+if [ -f "$MDTOOL" ] 
+then 
+  echo "Successfully found mdtool.exe file: $MDTOOL"
+else
+  echo "Configuration error. File $MDTOOL not found."
+  exit
+fi
+
+MDTOOL="$MONO $MDTOOL"
 
 # ------------------------------------------------------------------------------
 # Write Makefile
 
-sed -e "s,INSERT_MAKE_MDROOT,$MDDIR,g" -e "s,INSERT_MAKE_MDVERSION3,$MDVERSION3,g" -e "s,INSERT_MAKE_MDVERSION4,$MDVERSION4,g" Makefile.orig > Makefile 
+sed -e "s,INSERT_MAKE_MDROOT,$MDDIR,g" -e "s,INSERT_MAKE_MDTOOL,$MDTOOL,g" -e "s,INSERT_MAKE_MDVERSION,$MDVERSION,g" Makefile.orig > Makefile 
